e3f5b4e47c3dba94b5bd456909d516be53dd3795
ingdevfi <ingdevfi@gmail.com>
2018-01-25 20:41:06 +0100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Diagnostics;
+using System.Collections.Concurrent;
+
+namespace ClientHttp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+                        
+        }
+
+        private async void SendRequest(string url, ConcurrentBag<long> stats)
+        {
+            var sw = new Stopwatch();
+            sw.Start();
+            var client = new HttpClient();
+            client.DefaultRequestHeaders.Accept.Clear();
+            client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+
+            var stringTask = client.GetStringAsync(url);
+
+            var msg = await stringTask;
+            sw.Stop();
+            stats.Add(sw.ElapsedMilliseconds);
+        }
+    }
+}
